42fcde87f1a48e8cbbc1c843cb1ccfb70845f85d
cober2019 <53579603+cober2019@users.noreply.github.com>
2021-11-26 18:33:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def device_login() -> object:
 
 def menu() -> None:
 
-    print('\nXE-Routing Table\n')
+    print('\nRouting Tables\n')
     selection = input('1. XE-Routing\n2. ASA Routing\n3. NXOS Routing\n\nSelection: ')
     netmiko_connection = device_login()
 
